7164c80f6053651bca1e9ddc96f94fbd6f742ec4
Brandon Satrom <bsatrom@gmail.com>
2012-09-03 13:53:44 -0500

    additional work on Ads implementation

--- Tom8to/js/ads.js
+++ Tom8to/js/ads.js
@@ -15,12 +15,21 @@
         }
 
         el = document.getElementById(adElement);
+        backupEl = document.getElementById("backupAd");
 
-        var adsSdk = new MicrosoftNSJS.Advertising.AdControl(el, { applicationId: 'a0ac85c7-c493-4006-a9f2-d598d3e3dfcf', adUnitId: '10042356' }); 
-        
-        adsSdk.onErrorOccurred = function (sender, evt) {
-           //adsSdk = new MicrosoftNSJS.Advertising.AdControl(el, { applicationId: 'test_client', adUnitId: 'ImageText_320x50' });
-        };
+        var adsSdk = new MicrosoftNSJS.Advertising.AdControl(el, {
+            //applicationId: "a0ac85c7-c493-4006-a9f2-d598d3e3dfcf",
+            //adUnitId: "10042356",
+            applicationId: 'test_client', 
+            adUnitId: 'ImageText_320x50',
+            isAutoRefreshEnabled: false
+        });
+
+        adsSdk.refresh();
+
+        adsSdk.onErrorOccurred = function (elem, err) {
+            console.log("Ad error: " + err.errorMessage);
+        }
     }
 
     WinJS.Namespace.define("ADS", {
